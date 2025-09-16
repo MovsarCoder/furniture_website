3ee8ef113e0e64be8115bd8fa947e4e50d924d0c
DonviteCorleone <blackstar013.bs@gmail.com>
2025-09-17 01:06:45 +0300

    добавлена новая моделька Stats которая будет использоваться для редактирования информации

--- admin_service/admin.py
+++ admin_service/admin.py
@@ -1,19 +1,25 @@
 from unfold.admin import ModelAdmin
 from django.contrib import admin
-from .models import Review, Contact, Work
+from .models import Review, Contact, Work, Stats
 
 
 @admin.register(Work)
 class WorkAdmin(ModelAdmin):
-    list_display = ('title', 'price', 'currency', 'country', 'date', 'language', 'work_type', 'status', 'material', 'width', 'height', 'depth', 'created_at')
+    list_display = ('id', 'title', 'price', 'currency', 'country', 'date', 'language', 'work_type', 'status', 'material', 'width', 'height', 'depth', 'created_at')
     list_editable = ('price', 'currency', 'country', 'date', 'language', 'work_type', 'status', 'material', 'width', 'height', 'depth')
     list_filter = ('work_type', 'status', 'country', 'date', 'created_at', 'language',)
     search_fields = ('title', 'description', "date", "created_at", "status", "work_type", "language", 'country')
 
 
+@admin.register(Stats)
+class StatAdmin(ModelAdmin):
+    list_display = ("id", "clients_count", "projects_count", "years_experience", "delivery_weeks")
+    list_editable = ("projects_count", "years_experience", "delivery_weeks")
+
+
 @admin.register(Review)
 class ReviewAdmin(ModelAdmin):
-    list_display = ('author_name', 'text', 'date', 'language')
+    list_display = ('id', 'author_name', 'text', 'date', 'language')
     list_editable = ("text", "language")
     list_filter = ('language', 'date')
     search_fields = ('author_name', 'text', 'language')
@@ -21,7 +27,7 @@ class ReviewAdmin(ModelAdmin):
 
 @admin.register(Contact)
 class ContactAdmin(ModelAdmin):
-    list_display = ('branch_name', 'phone', 'email', 'address', "start_time", "end_time", 'country', 'language')
+    list_display = ('id', 'branch_name', 'phone', 'email', 'address', "start_time", "end_time", 'country', 'language')
     list_editable = ("phone", "email", "address", "start_time", "end_time", "country", "language")
     list_filter = ('country', 'language', "start_time", "end_time")
     search_fields = ('branch_name', 'address')
