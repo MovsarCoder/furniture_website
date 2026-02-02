2640cac693784b181f6048a9d8a4a87ee95100ee
DonviteCorleone <blackstar013.bs@gmail.com>
2026-02-03 01:09:41 +0300

    sdf

--- furniture/settings.py
+++ furniture/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-in-production')
 
-DEBUG = True
+DEBUG = False
 CSRF_TRUSTED_ORIGINS = ['https://bmass.at', "https://bmass.fr", "https://www.bmass.at", "https://www.bmass.fr", "https://127.0.0.1"]
 
 
